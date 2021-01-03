e233743909265f1b6fe8648667e38bb73f3b04ec
Melissa <melissakheying@gmail.com>
2021-01-03 15:24:38 -0600

    :construction: created Menu component and added logo

---
+++ client/src/components/views/menu/Menu.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './menu.scss';
+
+export const Menu = () => {
+  return (
+    <>
+
+      <div className="menu-header">
+
+      <body id='service-menu'>
+        <h1 className='service-menu-header'>Services</h1>
+        <div className="">
+          <div className="row">
+            <div className="col-3 col-s-3 menu">
+              <ul>
+                <li>Hi</li>
+                <li>Hi</li>
+                <li>Hi</li>
+                <li>Hi</li>
+              </ul>
+            </div>
+
+            <div className="col-6 col-s-9">
+            </div>
+
+            <div className="col-3 col-s-12">
+              <div className="menu-aside">
+                <ul>
+                  <li>Hi</li>
+                  <li>Hi</li>
+                  <li>Hi</li>
+                  <li>Hi</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+      </div>
+    </>
+  )
+}
