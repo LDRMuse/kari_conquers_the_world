68dd1d76a0906283629b787ef299731d31805d92
Melissa <melissakheying@gmail.com>
2021-02-17 10:25:17 -0600

    remove extra wording

--- client/src/components/views/Footer.js
+++ client/src/components/views/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className="footer">
-    <p>Resize the browser window to see how the content respond to the resizing.</p>
+
   </div>
   )
 }
